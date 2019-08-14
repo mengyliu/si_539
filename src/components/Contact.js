b75646e73795d7fccee000dcca55a234075cbf50
Mengyao Liu <mengyliu@umich.edu>
2019-08-14 03:05:51 -0400

    finish styling

---
+++ src/components/Contact.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import Fade from 'react-reveal/Fade';
+import data from '../data';
+
+class Contact extends Component {
+    state = {  }
+    render() { 
+        return (  <div>
+        <h1>
+                <Fade bottom cascade> Contact.</Fade>
+        </h1>
+        <Fade bottom>
+        <div className='contact-content'>
+                <h1>Having Fun?<br></br>
+                <span className='amazing-color'>Stay in Touch</span></h1>
+                    <a href={`mailto:${data.contactEmail}`} className='email'>{data.contactEmail}</a>
+                <ul>
+                    {data.social.map((link,index)=>(
+                        <li key={index}><a target='_blank' rel="noopener noreferrer" href={link.url}>{link.name}</a></li>
+                    ))}   
+                </ul>
+        </div>
+        </Fade>
+        </div>);
+    }
+}
+ 
+export default Contact;
