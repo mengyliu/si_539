94b0285ca23dc7aa7a3634d751fb519a35f4639c
Mengyao Liu <mengyliu@umich.edu>
2019-08-14 12:22:04 -0400

    Updates

--- precache-manifest.6a5cd5b0d90cc3248ab5ffea97d7f2a6.js
+++ precache-manifest.6a5cd5b0d90cc3248ab5ffea97d7f2a6.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "fe92707b302e252e7c34cbee9390bc0a",
+    "revision": "9013d58367237d24d8c62a14a97ee663",
     "url": "/si_539/index.html"
   },
   {
-    "revision": "a24fdbcbec7ca9d238b1",
+    "revision": "b54268e2aa8c2915d8af",
     "url": "/si_539/static/css/main.fc28e70d.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/si_539/static/js/2.19d9f5a4.chunk.js"
   },
   {
-    "revision": "a24fdbcbec7ca9d238b1",
-    "url": "/si_539/static/js/main.9132e85e.chunk.js"
+    "revision": "b54268e2aa8c2915d8af",
+    "url": "/si_539/static/js/main.2e6dfb39.chunk.js"
   },
   {
     "revision": "64c8cfb398a504e79ec4",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/si_539/precache-manifest.590ca802c190329993679d72ce32939b.js"
+  "/si_539/precache-manifest.6a5cd5b0d90cc3248ab5ffea97d7f2a6.js"
 );
 
 self.addEventListener('message', (event) => {
